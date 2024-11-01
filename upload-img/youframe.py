8f0f9d60318867271a06f3f6db527ae671370038
candi2002 <lethong2806@gmail.com>
2024-11-01 14:07:45 +0700

    Update youframe.py

--- upload-img/youframe.py
+++ upload-img/youframe.py
@@ -1,27 +1,31 @@
-import requests, os, re
+import os, re
 from flask import Flask, redirect, url_for, render_template, request
 from werkzeug.utils import secure_filename
 import socket
 
 app = Flask(__name__)
 
-# Hàm lấy địa chỉ IP động
 def get_ip_address():
-    # Lấy IP của máy chủ
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]  # Lấy IP nội bộ
+    except Exception:
+        ip_address = '127.0.0.1'
+    finally:
+        s.close()
     return ip_address
 
 @app.route("/")
 def uploader():
     path = 'static/uploads/'
-    uploads = sorted(os.listdir(path), key=lambda x: os.path.getctime(path + x))  # Sorting as per image upload date and time
+    uploads = sorted(os.listdir(path), key=lambda x: os.path.getctime(path + x))  # Sắp xếp theo thời gian
     uploads = ['uploads/' + file for file in uploads]
     uploads.reverse()
 
-    ip_address = get_ip_address()  # Lấy địa chỉ IP động của server
+    ip_address = get_ip_address()  # Lấy địa chỉ IP mạng nội bộ
 
-    return render_template("index.html", uploads=uploads, server_ip=ip_address)  # Truyền IP vào file HTML
+    return render_template("index.html", uploads=uploads, server_ip=ip_address)  # Truyền IP vào template
 
 app.config['UPLOAD_PATH'] = 'static/uploads'
 
