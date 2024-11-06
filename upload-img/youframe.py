9dcfe94d9a336a8ff636017830ed935aa42d87bb
candi2002 <lethong2806@gmail.com>
2024-11-07 01:27:00 +0700

    Update youframe.py

--- upload-img/youframe.py
+++ upload-img/youframe.py
@@ -1,10 +1,11 @@
-import os, re
+import os
 from flask import Flask, redirect, url_for, render_template, request
 from werkzeug.utils import secure_filename
 import socket
 
 app = Flask(__name__)
 
+# Hàm lấy địa chỉ IP
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -15,6 +16,19 @@ def get_ip_address():
     finally:
         s.close()
     return ip_address
+
+# Hàm tạo tên tệp duy nhất với hậu tố _1, _2, _3,...
+def get_unique_filename(filename):
+    name, ext = os.path.splitext(filename)
+    counter = 1
+    new_filename = filename
+
+    # Kiểm tra xem tên tệp đã tồn tại chưa, nếu tồn tại thì thêm hậu tố _1, _2, _3...
+    while os.path.exists(os.path.join(app.config['UPLOAD_PATH'], new_filename)):
+        new_filename = f"{name}_{counter}{ext}"
+        counter += 1
+    
+    return new_filename
 
 @app.route("/")
 def uploader():
@@ -27,6 +41,7 @@ def uploader():
 
     return render_template("index.html", uploads=uploads, server_ip=ip_address)  # Truyền IP vào template
 
+# Đặt đường dẫn lưu trữ tệp tải lên
 app.config['UPLOAD_PATH'] = 'static/uploads'
 
 @app.route("/upload", methods=['GET', 'POST'])
@@ -34,7 +49,8 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        unique_filename = get_unique_filename(filename)  # Tạo tên tệp duy nhất với hậu tố
+        f.save(os.path.join(app.config['UPLOAD_PATH'], unique_filename))  # Lưu tệp với tên duy nhất
         return redirect("/")
 
 if __name__ == "__main__":
